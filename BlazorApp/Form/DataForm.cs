f94ab221251736fc2bff001a456a37439e636bc3
lsurma <kontakt@lsurma.pl>
2023-01-26 22:00:10 +0100

    blazor

---
+++ BlazorApp/Form/DataForm.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+namespace BlazorApp.Form;
+
+public interface IFormDataModel
+{
+    
+}
+
+public interface IDataForm
+{
+}
+
+public class DataForm<T> : IDataForm
+{
+    public T Data { get; set; } = default(T)!;
+    
+    public List<DataFormField<T>> Fields { get; set; }
+}
+
+public class DataFormField<T> 
+{
+    public Expression<Func<T, object>> Path { get; set; }
+
+    public string Label { get; set; } = "";
+}
