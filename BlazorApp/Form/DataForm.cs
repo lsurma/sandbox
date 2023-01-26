1752a0758e187e606c528835d93d74921593bb28
lsurma <kontakt@lsurma.pl>
2023-01-26 23:08:46 +0100

    no message

--- BlazorApp/Form/DataForm.cs
+++ BlazorApp/Form/DataForm.cs
@@ -16,10 +16,16 @@ public class DataForm<T> : IDataForm
     public T Data { get; set; } = default(T)!;
     
     public List<DataFormField<T>> Fields { get; set; }
+    
+    
 }
 
-public class DataFormField<T> 
+public class DataFormField<T>
 {
+    public int Index { get; set; } = 0;
+    
+    public bool Repeatable { get; set; } = false;
+    
     public Expression<Func<T, object>> Path { get; set; }
 
     public string Label { get; set; } = "";
