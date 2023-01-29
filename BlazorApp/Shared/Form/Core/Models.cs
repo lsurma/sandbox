89856c72622df283044eddc2cbecc5aaed437179
lsurma <kontakt@lsurma.pl>
2023-01-29 22:39:52 +0100

    no message

---
+++ BlazorApp/Shared/Form/Core/Models.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq.Expressions;
+
+namespace BlazorApp.Shared.Form.Core;
+
+
+public class DynamicFormDefinition
+{
+    public List<DynamicFormFieldDefinition> Fields { get; set; }
+}
+
+public class DynamicFormFieldDefinition
+{
+    public bool Repeatable { get; set; }
+    
+    public Type? DataType { get; set; }
+    
+    public DynamicFormDefinition? NestedForm { get; set; }
+    
+    public LambdaExpression Path { get; set; }
+    
+    public string Label { get; set; }
+
+    public static DynamicFormFieldDefinition Create<T>(
+        Expression<Func<T, object>> path,
+        string? label = null,
+        bool? repeatable = false
+    )
+    {
+        return new DynamicFormFieldDefinition()
+        {
+            Path = path,
+            Label = label ?? "",
+            Repeatable = repeatable ?? false
+        };
+    }
+    
+}
